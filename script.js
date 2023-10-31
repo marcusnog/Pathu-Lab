e11fd190a5b5afdfabad273af279a244c29ead5a
marcusnog <marcusnogueira2105@gmail.com>
2023-10-31 17:33:10 -0300

    teste

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+
+    setTimeout(showSlides, 5000); // Troca de slide a cada 2 segundos (2000ms)
+}
